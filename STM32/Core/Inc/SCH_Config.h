6d8332c96d1badd4b60858bdb528321d557a60bf
trieu2201 <richdadtrieu@gmail.com>
2023-01-10 11:47:19 +0700

    SCH_Config and Scheduler header files

---
+++ STM32/Core/Inc/SCH_Config.h
@@ -0,0 +1,13 @@
+/*
+ * SCH_Config.h
+ *
+ *  Created on: Jan 10, 2023
+ *      Author: 84915
+ */
+
+#ifndef INC_SCH_CONFIG_H_
+#define INC_SCH_CONFIG_H_
+
+
+
+#endif /* INC_SCH_CONFIG_H_ */
---
+++ STM32/Core/Inc/Scheduler.h
@@ -0,0 +1,13 @@
+/*
+ * Scheduler.h
+ *
+ *  Created on: Jan 10, 2023
+ *      Author: 84915
+ */
+
+#ifndef INC_SCHEDULER_H_
+#define INC_SCHEDULER_H_
+
+
+
+#endif /* INC_SCHEDULER_H_ */
